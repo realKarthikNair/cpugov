c83df76130b2d53af1e5adc3bf6c75dcf38af1be
Karthik Nair <78267371+realKarthikNair@users.noreply.github.com>
2021-07-24 22:57:07 +0530

    Update cpugov.sh

--- cpugov.sh
+++ cpugov.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Cpu currently set to "
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo "which governor do u wanna use?"
 read governor
 echo $governor | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
