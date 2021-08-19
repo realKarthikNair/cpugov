a370425df7746cdaf99876c59ca235204528b9f1
Karthik Nair <78267371+realKarthikNair@users.noreply.github.com>
2021-08-19 17:45:17 +0530

    made better

--- cpugov.sh
+++ cpugov.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-echo "Cpu currently set to "
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo "which governor do u wanna use?"
+echo -e "Cpu currently set to \n"
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo -e "\nThe available governors are\n"
+cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors | tr ' ' '\n'
+echo -e "\nwhich governor do u wanna use?\n"
 read governor
 echo $governor | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo "governor set successfully"
+echo -e "\nGovernor $governor set successfully to all cores"
